c18358658009a6b4967c7c3749c99e051394f47c
Fabezo <92574746+FabezoN@users.noreply.github.com>
2026-06-22 11:16:05 +0200

    add Investigation Service

--- apps/investigations-service/src/app.module.ts
+++ apps/investigations-service/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BullModule } from '@nestjs/bullmq';
 import { HealthModule } from './modules/health/health.module';
 import { InvestigationsModule } from './modules/investigations/investigations.module';
 import { Investigation } from './modules/investigations/entities/investigation.entity';
+import { Report } from './modules/investigations/entities/report.entity';
 
 @Module({
   imports: [
@@ -17,8 +19,17 @@ import { Investigation } from './modules/investigations/entities/investigation.e
         username: config.getOrThrow('DATABASE_USER'),
         password: config.getOrThrow('DATABASE_PASSWORD'),
         database: config.getOrThrow('INVESTIGATIONS_DATABASE_NAME'),
-        entities: [Investigation],
+        entities: [Investigation, Report],
         synchronize: config.get('NODE_ENV') !== 'production',
+      }),
+    }),
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        connection: {
+          host: config.getOrThrow('REDIS_HOST'),
+          port: config.getOrThrow<number>('REDIS_PORT'),
+        },
       }),
     }),
     HealthModule,
--- apps/job-runner/src/job-runner.module.ts
+++ apps/job-runner/src/job-runner.module.ts
@@ -1,8 +1,40 @@
 import { Module } from "@nestjs/common";
-
-import { JobRunnerService } from "./job-runner.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { HttpModule } from "@nestjs/axios";
+import { BullModule } from "@nestjs/bullmq";
+import { Investigation } from "./entities/investigation.entity";
+import { Report } from "./entities/report.entity";
+import { InvestigationProcessor } from "./processors/investigation.processor";
 
 @Module({
-  providers: [JobRunnerService]
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: "postgres",
+        host: config.getOrThrow("DATABASE_HOST"),
+        port: config.getOrThrow<number>("DATABASE_PORT"),
+        username: config.getOrThrow("DATABASE_USER"),
+        password: config.getOrThrow("DATABASE_PASSWORD"),
+        database: config.getOrThrow("INVESTIGATIONS_DATABASE_NAME"),
+        entities: [Investigation, Report],
+        synchronize: false,
+      }),
+    }),
+    HttpModule,
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        connection: {
+          host: config.getOrThrow("REDIS_HOST"),
+          port: config.getOrThrow<number>("REDIS_PORT"),
+        },
+      }),
+    }),
+    BullModule.registerQueue({ name: "investigation-queue" }),
+  ],
+  providers: [InvestigationProcessor],
 })
 export class JobRunnerModule {}
